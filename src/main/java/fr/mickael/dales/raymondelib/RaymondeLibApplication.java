2d1068026e6e151d3a7024236d3009e18fd50dca
mdales <mdales@ippon.fr>
2021-04-14 19:20:40 +0200

    init spring project

---
+++ src/main/java/fr/mickael/dales/raymondelib/RaymondeLibApplication.java
@@ -0,0 +1,13 @@
+package fr.mickael.dales.raymondelib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaymondeLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaymondeLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/mickael/dales/raymondelib/RaymondeLibApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mickael.dales.raymondelib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaymondeLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
